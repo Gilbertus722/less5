7b33a240c6af23559a0643fc2d17fa5e2530fa52
Vitalii <gilbertus722@yandex.ru>
2022-09-19 21:00:04 +0300

    Задача 34а1

--- DZ34a/Program.cs
+++ DZ34a/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine($"Четные числа в массиве");
-int[] numbers = new int[10];
+int[] num = new int[10];
 
 void FillArray(int[] array, int min, int max){
   for (int i = 0; i<array.Length; i++ ){
@@ -25,9 +25,9 @@ int QuantityPositive(int[] array){
   return quantity;
 }
 
-FillArray(numbers, 100, 1000);
-WriteArray(numbers);
+FillArray(num, 100, 1000);
+WriteArray(num);
 Console.WriteLine();
 
-int quantity = QuantityPositive(numbers);
+int quantity = QuantityPositive(num);
 Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
